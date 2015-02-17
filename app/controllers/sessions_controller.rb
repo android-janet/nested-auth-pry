4f285c7b0c45fa63d67deae2ac0d53e100469f44
Melissa Khat <melissa.khat@gmail.com>
2015-02-16 17:29:35 -0700

    user can sign in, sign out, and sign up from home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,21 @@
 class SessionsController < ApplicationController
-    def signin
-      
+    def new
+
+    end
+
+    def create
+      @user = User.find_by_email(params[:email])
+      if @user and @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to root_path, notice: "Successfully logged in!"
+      else
+        flash.now[:message] = "Invalid email/password."
+        render :new
+      end
+    end
+
+    def destroy
+      session[:user_id] = nil
+      redirect_to root_path, notice: "Successfully logged out!"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   root 'companies#index'
   get '/signup'=>'users#new'
   post '/signup'=>'users#create'
+  get '/signin'=>'sessions#new'
+  post '/signin'=>'sessions#create'
+  get '/signout'=>'sessions#destroy'
 
 end
